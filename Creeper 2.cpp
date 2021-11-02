b424b9fbfb0643785d9a5af72eb7323638fda582
Dragon-uf <89758733+Dragon-uf@users.noreply.github.com>
2021-11-02 17:45:25 -0300

    Add files via upload

---
+++ Creeper 2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<windows.h>
+using namespace  std;
+
+int main(void)
+{
+	
+	     HANDLE hConsole = GetStdHandle( STD_OUTPUT_HANDLE );
+ SetConsoleTextAttribute(hConsole, 2);
+	cout<<"\n."<<endl;
+	cout<<"\t\t\t\Hello i'm creeper catch me if you can"<<endl;
+	
+     cout<<"\n\t\t\you will no longer be able to open this application"<<endl;
+	
+	cout<<"\n\t\t\t\t\accessing the data"<<endl;
+	
+	
+	
+	cout<<"\n \t\give it to exit or if not I will damage your pc"<<endl;
+	 
+	
+	cout<<"\n0. EXIT"<<endl;
+	
+
+	return 0;
+	
+	
+}
